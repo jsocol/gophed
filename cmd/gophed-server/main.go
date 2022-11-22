e550339aedc459f18a5c378b6ec3ed7e7b18b219
James Socol <me@jamessocol.com>
2022-11-21 23:12:10 -0500

    Add graceful shutdown

--- cmd/gophed-server/main.go
+++ cmd/gophed-server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/jsocol/gophed/handlers"
 	"github.com/jsocol/gophed/repository"
@@ -24,9 +28,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("listening on %s\n", addr)
 
-	if err := http.Serve(listener, mux); err != nil {
-		log.Fatal(err)
+	srv := http.Server{
+		Handler:           mux,
+		ReadHeaderTimeout: 1 * time.Second,
 	}
+
+	serverClosed := make(chan struct{}, 1)
+	go func() {
+		log.Printf("Listening on %s...\n", addr)
+		if err := srv.Serve(listener); err != http.ErrServerClosed {
+			log.Fatalln(err)
+		}
+		close(serverClosed)
+	}()
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		log.Println("Shutting down...")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		srv.Shutdown(ctx)
+	}()
+
+	<-serverClosed
 }
